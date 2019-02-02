3259c0474f85b0c6ea7841d098f6537699cdab05
Josh Feldman <joshfeldman95@gmail.com>
2019-02-02 13:42:52 -0500

    added package architecture:

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nlp_models',
+      version='0.1',
+      description='NLP models for cs287',
+      url='https://github.com/JoshFeldman95/nlp_models',
+      author='Josh Feldman',
+      author_email='JoshFeldman95',
+      license='MIT',
+      packages=['nlp_models'],
+      zip_safe=False)
